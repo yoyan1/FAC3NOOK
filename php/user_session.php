087fc761a185e51e2dc78c7b3251143efdfe5beb
yoyan1 <yoyan947@gmail.com>
2024-02-17 19:16:54 +0800

    first commit

--- php/user_session.php
+++ php/user_session.php
@@ -7,6 +7,14 @@ session_start();
         $query = mysqli_query($conn, "SELECT * FROM user WHERE id = '$id' ") or die("query failed");
         $user = mysqli_fetch_assoc($query);
 
+        $online = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM `browse_type` WHERE user = '$id'"));
+
+        if($online['status'] == 'OFFLINE'){
+            header("location: ../login.php");
+        } else{
+            
+        }
+
         $_SESSION['id'] = $id;
     } else{
         header("location: ../login.php");
